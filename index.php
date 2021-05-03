6b7255e053a34c0bd4ba129b9ecdb3a75deb5bad
Tommaso P <tommaso.plinio@gmail.com>
2021-05-03 16:31:35 +0200

    testo

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// variabili
+$text = 'lorem ipsum eccmrhhua ajgtsy vbernao dhuc bnnao'; 
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php badwords</title>
+</head>
+<body>
+    <p><?php echo $text; ?></p>
+    <p>Il testo ha una lunghezza di <?php echo strlen($text)?> caratteri</p>
+</body>
+</html>
